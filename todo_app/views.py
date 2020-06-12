3560f2a7d027e56f63645fba94e41d5eb1058260
LongYang <julyzergcn1@gmail.com>
2020-06-12 23:00:00 +0800

    bugfix json ajax return

--- todo_app/views.py
+++ todo_app/views.py
@@ -6,6 +6,18 @@ from django.http.response import JsonResponse
 from . import models
 
 
+def get_todo_items():
+    qs = models.TodoItem.objects.filter(deleted=False)
+    return [
+        {
+            'id': obj.id,
+            'title': obj.title,
+            'completed': obj.completed,
+        }
+        for obj in qs
+    ]
+
+
 def home(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -24,9 +36,11 @@ def home(request):
         models.TodoItem.objects.filter(deleted=False)\
             .exclude(id__in=ids).update(deleted=True)
         models.TodoItem.objects.bulk_create(item_objs)
-        return JsonResponse({'ret': 'ok'})
+
+        return JsonResponse({'todo_items': get_todo_items()})
 
     context = {
-        'todo_items': models.TodoItem.objects.filter(deleted=False),
+        'todo_items': json.dumps(get_todo_items()),
     }
+
     return render(request, 'todo_home.html', context)
